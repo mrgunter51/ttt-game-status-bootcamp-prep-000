48ad7263748d4c56a43369bea5ad125c2f30ba35
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-26 09:02:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,8 @@ def won?(board)
     if position_taken?(board,combo[0]) && position_taken?(board,combo[1]) && position_taken?(board,combo[2])
       if board[combo[0]] == board[combo[1]]  && board[combo[1]] == board[combo[1]]
         return combo
+      else
+        false
       end
     else
       false
